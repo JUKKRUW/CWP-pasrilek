d32495385d4a5dc0fed6148f6b9b6dc2a9bba394
Paramate <paramaet.oun@gmail.com>
2026-09-22 19:50:48 +0700

    ex00 01

---
+++ discovery_piscine/cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Paramate"
+last_name = "Srilek"
+print(first_name + " "+ last_name)
--- discovery_piscine/cell01/ex01/name.py
+++ discovery_piscine/cell01/ex01/name.py
@@ -1,3 +1,4 @@
 first_name = "Paramate"
 last_name = "Srilek"
-print(first_name + " "+ last_name)
+whole_name = first_name + " "+ last_name
+print(whole_name)
