19def80a95d49a9c168c4aa6b1e97eca3c25c397
Paramate <paramaet.oun@gmail.com>
2026-09-23 16:28:41 +0700

    cell03 ex02

--- discovery_piscine/cell03/ex02/i_got_that.py
+++ discovery_piscine/cell03/ex02/i_got_that.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
 user_input = input("What you gotta say? : ")
-
-while True:
-    if user_input == "STOP":
-        break
+while user_input != "STOP":
     user_input = input("I got that! Anything else? : ")
