fcb56a36a8b20c57eca1ca2e599b824b665f81f6
Paramate <paramaet.oun@gmail.com>
2026-09-23 14:11:03 +0700

    ex03

--- discovery_piscine/cell02/ex03/mult.py
+++ discovery_piscine/cell02/ex03/mult.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+
 num1 = int(input("Enter the first number:\n"))
 num2 = int(input("Enter the second number:\n"))
-print(f"{num1} x {num2} = {num1*num2}")
-if num1*num2 > 0 :
+result = num1 * num2
+
+print(f"{num1} x {num2} = {result}")
+
+if result > 0:
     print("The result is positive.")
-elif num1*num2 < 0 :
+elif result < 0:
     print("The result is negative.")
-else: 
-    print("The result is positive and negtive.")
+else:
+    print("The result is positive and negative.")
